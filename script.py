ce1cfd0a68d161dd5a2a58cf551708332d1f1deb
marionmorel <marion.mrl@live.fr>
2026-02-01 13:36:04 +0000

    Task 1

--- script.py
+++ script.py
@@ -2,3 +2,7 @@ names = ["Mohamed", "Sara", "Xia", "Paul", "Valentina", "Jide", "Aaron", "Emily"
 insurance_costs = [13262.0, 4816.0, 6839.0, 5054.0, 14724.0, 5360.0, 7640.0, 6072.0, 2750.0, 12064.0]
 
 # Add your code here
+
+# Task 1
+names.append("Priscilla")
+insurance_costs.append(8320.0)
